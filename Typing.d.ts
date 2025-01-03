7a02e3c4f60019ea8cb06cb749ee54a1601dce13
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2025-01-03 13:09:55 +0500

    milestone

---
+++ Typing.d.ts
@@ -0,0 +1,6 @@
+type Post = {
+    title: string,
+    summary: string,
+    image: never,
+    slug: string,
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: "#f9fafb",
+        dark: "#1b1b1b",
+        primary: "#b63e96", // Fixed the double #
+        primaryDark: "#58e6d9", // Fixed the double #
+        accentDarkPrimary: "#a020f0",
+        accentDarkSecondary: "#f0a020",
+      },
+      screens: {
+        xs: "479px",
+        sm: "639px",
+        md: "767px",
+        lg: "1023px",
+        xl: "1280px",
+        "2xl": "1535px",
       },
     },
   },
